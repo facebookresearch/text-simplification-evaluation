e0f730fb7ace1b4a7bffc03be7ca86feba47212b
Louis MARTIN <louisrtm@gmail.com>
2019-07-05 09:46:15 -0700

    Post install cmd to download nltk stuff

--- setup.py
+++ setup.py
@@ -6,17 +6,43 @@
 #
 
 from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+from setuptools.command.install import install
 
 
-setup(name='tseval',
-      version='1.0',
-      description='Reference-less Text Simplification Evaluation Methods',
-      author='Louis Martin',
-      author_email='louismartin@fb.com',
-      include_package_data=True,
-      install_requires=[
-          'numpy>=1.1.15', 'scipy', 'pandas', 'torch', 'sklearn', 'python-Levenshtein', 'gitpython',
-          'nlg-eval@git+https://github.com/Maluuba/nlg-eval.git',
-          'fairseq@git+https://github.com/pytorch/fairseq@e286243c68f1589a781488580fc19388714612be',
-          ],
-      packages=find_packages())
+def _post_setup():
+    from nltk.downloader import download
+    download('stopwords')
+
+
+# Set up post install actions as per https://stackoverflow.com/a/36902139/1226799
+class PostDevelopCommand(develop):
+    def run(self):
+        develop.run(self)
+        _post_setup()
+
+
+class PostInstallCommand(install):
+    def run(self):
+        install.run(self)
+        _post_setup()
+
+
+setup(
+    name='tseval',
+    version='1.0',
+    description='Reference-less Text Simplification Evaluation Methods',
+    author='Louis Martin',
+    author_email='louismartin@fb.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'numpy>=1.1.15', 'scipy', 'pandas', 'torch', 'sklearn', 'python-Levenshtein', 'gitpython',
+        'nlg-eval@git+https://github.com/Maluuba/nlg-eval.git',
+        'fairseq@git+https://github.com/pytorch/fairseq@e286243c68f1589a781488580fc19388714612be',
+        ],
+    cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+    },
+)
